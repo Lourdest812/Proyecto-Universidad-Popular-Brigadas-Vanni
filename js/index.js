6ab95a7f026fee13084f86d687caf69aafb52985
Lourdes Toledo <lourdest812@gmail.com>
2023-05-29 00:16:03 -0300

    Cambios finales

--- js/index.js
+++ js/index.js
@@ -44,3 +44,34 @@ for (var i = 1; i <= 5; i++) {
   );
 }
 
+
+//FORMULARIO DE CONTACTO
+
+function habilitar(){
+  campo1 = document.getElementById("campo1").value;
+  campo2 = document.getElementById("campo2").value;
+  campo3 = document.getElementById("campo3").value;
+  val = 0;
+  
+  if(campo1 == ""){
+      val++; 
+  }
+  if(campo2 == ""){
+      val++; 
+  }
+  if(campo3 == ""){
+      val++; 
+  }
+  
+  if(val == 0){
+      document.getElementById("enviar").disabled = false;
+  }else{
+      document.getElementById("enviar").disabled = true;
+  }
+   
+  }
+  
+  document.getElementById("campo1").addEventListener("keyup", habilitar);
+  document.getElementById("campo2").addEventListener("keyup", habilitar);
+  document.getElementById("campo3").addEventListener("keyup", habilitar);
+
