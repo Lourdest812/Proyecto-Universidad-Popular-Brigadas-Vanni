213cb867693ccc9ced422042474e6e2b50afe3f0
Lourdes Toledo <lourdest812@gmail.com>
2023-05-29 01:00:38 -0300

    Cambios finales

--- js/index.js
+++ js/index.js
@@ -27,3 +27,21 @@
   getRandomUser();
 
 
+  //GALERÍA DE IMAGENES EN SOBRE NOSOTROS
+var imageThumbs = document.getElementById("image-thumbs");
+var currentImage = document.getElementById("current-image");
+
+for (var i = 1; i <= 5; i++) {
+  var thumb = document.createElement("img");
+  thumb.src = "img/image" + i + ".jpg";
+  thumb.alt = "Image " + i;
+  thumb.classList.add("thumb");
+  imageThumbs.appendChild(thumb);
+  thumb.addEventListener(
+    "click", function() {
+    currentImage.src = this.src;
+    }
+  );
+}
+
+
